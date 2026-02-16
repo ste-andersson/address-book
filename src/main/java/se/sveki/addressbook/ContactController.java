be3a1ad1e4feea2a068d3e5f4bb5ef701ac6e3ab
Stefan Andersson <stefan.andersson@sveki.se>
2026-02-16 20:16:25 +0100

    Add endpoint in ContactController for POST to create contacts

--- src/main/java/se/sveki/addressbook/ContactController.java
+++ src/main/java/se/sveki/addressbook/ContactController.java
@@ -1,5 +1,7 @@
 package se.sveki.addressbook;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -11,4 +13,17 @@ import java.util.concurrent.atomic.AtomicLong;
 public class ContactController {
     private final AtomicLong counter = new AtomicLong();
     private final Map<Long, Contact> db = new ConcurrentHashMap<>();
+
+    @PostMapping("/api/contacts")
+    public Contact create(@RequestParam String firstName,
+                          @RequestParam String lastName,
+                          @RequestParam String country,
+                          @RequestParam String city,
+                          @RequestParam String streetName,
+                          @RequestParam String streetNumber) {
+        long id = counter.incrementAndGet();
+        Contact created = new Contact(id, firstName, lastName, country, city, streetName, streetNumber);
+        db.put(id, created);
+        return created;
+    }
 }
